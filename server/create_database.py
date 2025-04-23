c0306ab9d089c8370a379df1140a391836d8c78e
JB-Rose <joshbrose1@gmail.com>
2025-04-22 17:38:46 -0700

    Added statements to upload dummy resume and job description for testing

--- server/create_database.py
+++ server/create_database.py
@@ -38,12 +38,35 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS scores (
 );
 """)
 
+#Shouldnt we add an imrpovement keyword to this?
 cursor.execute("""CREATE TABLE IF NOT EXISTS improvements (
     session_id INT PRIMARY KEY,
     improvement TEXT,
     reference_text TEXT
 );
 """)
+
+#FOR TESTING!!!!!
+# cursor.execute("""INSERT INTO improvements (session_id, improvement, reference_text) VALUES
+# (1, 'misspelled', 'apple'),
+# (2, 'repeated word', 'banana');
+# """)
+
+with open('pdfs/resume_john.pdf', 'rb') as file1, open('pdfs/resume_alice.docx', 'rb') as file2:
+    file1_data = file1.read()
+    file2_data = file2.read()
+
+cursor.execute("""INSERT INTO resumes (session_id, file_name, file_data) VALUES
+(1, 'resume_bob.pdf', %s),
+(2, 'resume_alice.docx', %s);
+""", (file1_data, file2_data))
+
+cursor.execute("""INSERT INTO job_description (session_id, job_description) VALUES
+(1, 'move boxes'),
+(2, 'SWE');
+""")
+
+
 
 connection.commit()
 
